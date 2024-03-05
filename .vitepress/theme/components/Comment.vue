fd7267935735e43e3a78655a9f38542f4b0a6ea3
lyn <Lyndon_lilin@123.com>
2024-03-05 16:30:33 +0800

    fix: route path change, gitalk not init

--- .vitepress/theme/components/Comment.vue
+++ .vitepress/theme/components/Comment.vue
@@ -1,18 +1,18 @@
 <template>
-  <div id="comment-container"></div>
+  <div v-if="showFlag" id="comment-container"></div>
 </template>
 
 <script lang="ts" setup>
-import { onMounted } from 'vue'
+import { onMounted, watch, ref, nextTick } from 'vue'
 import 'gitalk/dist/gitalk.css'
 import Gitalk from 'gitalk'
-import { useData } from 'vitepress'
+import { useData, useRouter } from 'vitepress'
 import md5 from 'blueimp-md5'
 
 const { page } = useData()
+const { route } = useRouter()
 
-// 初始化评论组件配置
-let gitalk = new Gitalk({
+const gitDefault = {
   clientID: '013e5673e91fe10c293f',
   clientSecret: 'bb3a3512d7ec932e47f082b955658861d3b45f87',
   repo: 'vitepress-blog-gitalk',
@@ -23,38 +23,55 @@ let gitalk = new Gitalk({
   distractionFreeMode: false,
   // 默认: https://cors-anywhere.azm.workers.dev/https://github.com/login/oauth/access_token
   proxy: 'https://vercel.charles7c.top/github_access_token'
-})
+}
 
+const showFlag = ref(true)
 // 渲染评论组件
 onMounted(() => {
-  gitalk.render('comment-container')
-  // 点赞前检查登录状态
-  const commentContainer: HTMLElement | null =
-    document.getElementById('comment-container')
+  watch(
+    () => route.path,
+    () => {
+      showFlag.value = false
+      nextTick(() => {
+        showFlag.value = true
+        setTimeout(() => {
+          // 初始化评论组件配置
+          const gitalk = new Gitalk({ ...gitDefault, id: md5(page.value.relativePath) })
 
-  commentContainer?.addEventListener('click', (event: Event) => {
-    if (!window.localStorage.getItem('GT_ACCESS_TOKEN')) {
-      alert('点赞前，请先登录')
-      event.preventDefault()
-    }
-  })
+          gitalk.render('comment-container')
+          // 点赞前检查登录状态
+          const commentContainer: HTMLElement | null =
+            document.getElementById('comment-container')
 
-  // 提交评论后重置输入框高度
-  commentContainer?.addEventListener('click', (event: Event) => {
-    const gtTextarea: HTMLElement | null = document.querySelector('.gt-header-textarea')
-    if (gtTextarea) {
-      ;(gtTextarea as HTMLInputElement).style.height = '72px'
-    }
-  })
+          commentContainer?.addEventListener('click', (event: Event) => {
+            if (!window.localStorage.getItem('GT_ACCESS_TOKEN')) {
+              alert('点赞前，请先登录')
+              event.preventDefault()
+            }
+          })
 
-  // 点击预览时切换评论按钮可见性
-  commentContainer?.addEventListener('click', (event: Event) => {
-    const commentButton: HTMLElement | null = document.querySelector(
-      '.gt-header-controls .gt-btn-public'
-    )
-    if (commentButton) {
-      commentButton.classList.toggle('hide')
-    }
-  })
+          // 提交评论后重置输入框高度
+          commentContainer?.addEventListener('click', (event: Event) => {
+            const gtTextarea: HTMLElement | null =
+              document.querySelector('.gt-header-textarea')
+            if (gtTextarea) {
+              ;(gtTextarea as HTMLInputElement).style.height = '72px'
+            }
+          })
+
+          // 点击预览时切换评论按钮可见性
+          commentContainer?.addEventListener('click', (event: Event) => {
+            const commentButton: HTMLElement | null = document.querySelector(
+              '.gt-header-controls .gt-btn-public'
+            )
+            if (commentButton) {
+              commentButton.classList.toggle('hide')
+            }
+          })
+        }, 0)
+      })
+    },
+    { immediate: true }
+  )
 })
 </script>
