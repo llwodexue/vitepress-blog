a5500674e14150db6980c48ddc3208282e80f04a
lyn <Lyndon_lilin@123.com>
2024-04-29 16:31:02 +0800

    feat: pwa config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,62 +3,67 @@ import { sidebar, nav } from './sidebar'
 // import algolia from './config/algolia'
 import markdown from './config/markdown'
 import { name, keywords } from './config/meta'
+import { withPwa } from '@vite-pwa/vitepress'
+import { pwa } from './config/pwa'
 
 const base = process.env.BASE || '/'
 
-export default defineConfig({
-  // outDir: '../dist',
-  base,
-  title: name,
-  lastUpdated: true,
-  cleanUrls: true,
-  locales: {
-    root: { label: '简体中文', lang: 'zh-CN' }
-  },
-  vite: {
-    build: {
-      chunkSizeWarningLimit: 1500
-    }
-  },
-  sitemap: {
-    hostname: 'https://www.llmysnow.top/'
-  },
-  markdown: markdown,
-  themeConfig: {
-    search: {
-      provider: 'local'
+export default withPwa(
+  defineConfig({
+    pwa,
+    // outDir: '../dist',
+    base,
+    title: name,
+    lastUpdated: true,
+    cleanUrls: true,
+    locales: {
+      root: { label: '简体中文', lang: 'zh-CN' }
     },
-    logo: '/avatar.png',
-    docFooter: {
-      prev: '上一篇',
-      next: '下一篇'
+    vite: {
+      build: {
+        chunkSizeWarningLimit: 1500
+      }
     },
-    returnToTopLabel: '返回顶部',
-    outlineTitle: '导航栏',
-    darkModeSwitchLabel: '外观',
-    sidebarMenuLabel: '归档',
-    lastUpdatedText: '最后一次更新于',
-    outline: {
-      level: 'deep',
-      label: '目录'
+    sitemap: {
+      hostname: 'https://www.llmysnow.top/'
     },
-    socialLinks: [{ icon: 'github', link: 'https://github.com/llwodexue' }],
-    footer: {
-      message: '常备不懈，才能有备无患'
-      // copyright: `版权所有 © 2022-${new Date().getFullYear()}`
+    markdown: markdown,
+    themeConfig: {
+      search: {
+        provider: 'local'
+      },
+      logo: '/avatar.png',
+      docFooter: {
+        prev: '上一篇',
+        next: '下一篇'
+      },
+      returnToTopLabel: '返回顶部',
+      outlineTitle: '导航栏',
+      darkModeSwitchLabel: '外观',
+      sidebarMenuLabel: '归档',
+      lastUpdatedText: '最后一次更新于',
+      outline: {
+        level: 'deep',
+        label: '目录'
+      },
+      socialLinks: [{ icon: 'github', link: 'https://github.com/llwodexue' }],
+      footer: {
+        message: '常备不懈，才能有备无患'
+        // copyright: `版权所有 © 2022-${new Date().getFullYear()}`
+      },
+      // algolia,
+      nav,
+      sidebar
     },
-    // algolia,
-    nav,
-    sidebar
-  },
-  head: [
-    // not never(no-referer), image cannot be accessed
-    ['meta', { name: 'referrer', content: 'never' }],
-    // must be referer, get the number of articles accessed
-    // ['meta', { name: 'referrer', content: 'no-referrer-when-downgrade' }],
-    ['meta', { name: 'keywords', content: keywords }],
-    ['meta', { name: 'author', content: 'lyn' }],
+    head: [
+      // not never(no-referer), image cannot be accessed
+      ['meta', { name: 'referrer', content: 'never' }],
+      // must be referer, get the number of articles accessed
+      // ['meta', { name: 'referrer', content: 'no-referrer-when-downgrade' }],
+      ['meta', { name: 'keywords', content: keywords }],
+      ['meta', { name: 'author', content: 'lyn' }],
 
-    ['link', { rel: 'icon', type: 'image/svg+xml', href: base + 'rem.svg' }]
-  ]
-})
+      ['link', { rel: 'icon', type: 'image/svg+xml', href: base + 'rem.svg' }]
+    ]
+  })
+)
---
+++ .vitepress/config/pwa.ts
@@ -0,0 +1,41 @@
+import type { VitePWAOptions } from 'vite-plugin-pwa'
+import fg from 'fast-glob'
+import { resolve } from 'path'
+import { name, keywords } from './meta'
+
+/**
+ * Vite Plugin PWA uses Workbox  library to build the service worker
+ * can find more information on Workbox section.
+ * @see https://vite-plugin-pwa.netlify.app/
+ */
+export const pwa: Partial<VitePWAOptions> = {
+  // outDir: '../dist',
+  registerType: 'autoUpdate',
+  includeAssets: fg.sync('**/*.{png,svg,gif,ico,txt}', {
+    cwd: resolve(__dirname, '../../public')
+  }),
+  manifest: {
+    id: '/',
+    name,
+    short_name: name,
+    description: keywords,
+    theme_color: '#009ff7',
+    icons: [
+      {
+        src: '/icons/rem128x128.png',
+        sizes: '128x128',
+        type: 'image/png'
+      },
+      {
+        src: '/icons/rem192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: '/icons/icons/rem512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+    ]
+  }
+}
